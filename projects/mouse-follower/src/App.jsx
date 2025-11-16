0603cb43c2e392e1e2e5194708fb2d890af2021e
WilcarOrtiz <ortizcolpaswilcardaniel@gmail.com>
2025-11-16 16:46:04 -0500

    actualizacion de mouse-follower

--- projects/mouse-follower/src/App.jsx
+++ projects/mouse-follower/src/App.jsx
@@ -1,8 +1,53 @@
-function App() {
+import { useState, useEffect } from 'react'
+
+const FollowMouse = () => {
+  const [enable, SetEnable] = useState(false)
+  const [position, SetPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('Hola mundo', { enable })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      SetPosition({ x: clientX, y: clientY })
+    }
+    if (enable) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      // clean the suscription
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enable])
   return (
     <>
-      <h1>hola</h1>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px,${position.y}px)`,
+        }}
+      ></div>
+      <button onClick={() => SetEnable(!enable)}>
+        {enable ? 'Desactivar' : 'Activar'}
+      </button>
     </>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
